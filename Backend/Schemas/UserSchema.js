0d4b8c72a97a4e449f2a9cf9f4d97819153fe142
Max <mn27304@gmail.com>
2020-12-16 13:32:01 +0100

    Register user step 1

--- Backend/Schemas/UserSchema.js
+++ Backend/Schemas/UserSchema.js
@@ -3,12 +3,17 @@ const { Schema } = mongoose;
 
 const UserSchema = new Schema({
   name: String,
+  nickName: String,
   email: { type: String, required: true },
   password: { type: String, required: true },
   role: {
     type: String,
     enum: ["user", "admin"],
     default: "user",
+  },
+  confirmed: {
+    type: Boolean,
+    default: false,
   },
 });
 
